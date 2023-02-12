e30b6a1569a374be43ac03709e042edf652486ef
Roch <rochishnu7@gmail.com>
2023-02-12 21:25:55 +0530

    factorial of a number using recursion & iteration

--- DS/main.cpp
+++ DS/main.cpp
@@ -9,16 +9,22 @@
 using namespace std;
 
 
-int sum( int num ){
-    if (num == 0) return 0;
-    return sum( num - 1 ) + num ;
+int fac( int num ){
+    if (num==0) return 1;
+    return fac( num-1 )*num;
+}
+
+int ifac( int num ){
+    int prod=1;
+    for ( int i = 1 ; i<=num ; i++ ){
+        prod = prod * i;
+    }
+    return prod;
 }
 
 
 int main(int argc, const char * argv[]) {
-    cout << sum(5);
-    
-    
+    cout << ifac(5)<<endl;
     return 0;
     
 }
