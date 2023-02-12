d1598ab4b11b1089b037f771bb2c3e5b5dd2fe1d
Roch <rochishnu7@gmail.com>
2023-02-12 23:24:00 +0530

    power function

--- DS/main.cpp
+++ DS/main.cpp
@@ -9,22 +9,23 @@
 using namespace std;
 
 
-int fac( int num ){
-    if (num==0) return 1;
-    return fac( num-1 )*num;
+int pow( int m , int n ){
+    if ( m==0) return 0;
+    if ( n==0 ) return 1;
+    return  pow ( m , n-1 )*m;
 }
 
-int ifac( int num ){
-    int prod=1;
-    for ( int i = 1 ; i<=num ; i++ ){
-        prod = prod * i;
-    }
-    return prod;
+int powOptimized( int m ,int n ){
+    if ( m == 0 ) return 0;
+    if ( n==0 ) return 1;
+    
+    if ( n%2==0 ) return pow( m*m, n/2 ); // 2^6 -> 2^3 *2^ 3
+    else return pow( m*m , (n-1)/2 )*m;
 }
 
 
 int main(int argc, const char * argv[]) {
-    cout << ifac(5)<<endl;
+    cout << powOptimized(5, 3)<<endl;
     return 0;
     
 }
