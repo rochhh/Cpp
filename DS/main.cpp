d89313292a6916679373b58c597264f96566d846
Roch <rochishnu7@gmail.com>
2023-03-06 00:57:36 +0530

    Stack impl using array

--- DS/main.cpp
+++ DS/main.cpp
@@ -6,32 +6,61 @@
 //
 
 #include <iostream>
-#include <cmath>
+#include <stdlib.h>
 using namespace std;
 
-/**
- is power of two Leetcode
- */
+struct Stack {
+    int size;
+    int top;
+    int *s;
+};
 
-int main(int argc, const char * argv[]) {
-    
-    int *p;
-    p = new int[2];
-    p[0] = 33;
-    p[1] = 45;
-    p[2] = 89;
-    int *q;
-    q = new int[4];
-    for ( int i = 0 ; i <=4 ; i++ ){
-        q[i] = p[i];
+void push( Stack *st , int value ){
+    if ( st->top == st->size-1 ) {
+        cout << "Stack overflow" << endl;
     }
-    free(p);
-    p=q;
-    q=NULL;
-    
-    for ( int i = 0 ; i <= 4  ; i ++ ){
-        cout << p[i] << endl ;
+    else{
+        st->top++;
+        st->s[st->top]=value;
     }
+}
+        
+int pop( Stack *st){
+    int x = 0;
+    if ( st->top == -1 ) cout << "Stack is empty" << endl;
+    x = st->s[st->top];
+    st->top--;
+    
+    return x;
+}
+
+int stackTop( Stack *st ){
+    if ( st->top == -1 ) return -1;
+    
+    return st->s[st->top];
+}
+
+int isEmpty( Stack *st ){
+    if ( st->top == -1 ) return 1;
     return 0;
+}
+
+int isFull( Stack *st ){
+    if ( st->top == st->size-1 ) return 1;
+    return 0;
+}
+
+int peek( Stack st , int pos ){
+    int x = -1;
     
+    if ( st.top-pos+1 < 0 ) cout << "no you're doing it wrong fam";
+    
+    x = st.s[st.top-pos+1];
+    
+    return x;
+}
+
+int main(){
+    
+    return 0;
 }
