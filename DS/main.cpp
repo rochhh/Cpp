84b48be537910173a99103e221081e0dbb1d82d9
Roch <rochishnu7@gmail.com>
2023-03-07 22:18:26 +0530

    queue impl started using arrays

--- DS/main.cpp
+++ DS/main.cpp
@@ -9,98 +9,33 @@
 #include <stdlib.h>
 using namespace std;
 
-struct Stack {
+struct Queue{
     int size;
-    int top;
-    int *s;
+    int front;
+    int rear;
+    int *Q;
 };
 
-bool isBalanced( char *exp ){
-//    struct Stack st;
-//    st.top = -1;
-//    st.size = strlen(exp);
-//    st.s = new char[exp.size()];
-    for ( int i = 0 ; exp[i] < '\0'; i++ ){
-        if ( exp[i] == '(' ) {
-            push(exp[i]);
-        }
-        if ( exp[i] == ')' ){
-            if ( top == NULL ){
-                return 0;
-                
-            } pop();
-        }
-        
-        if ( top == Null ) return 1;
-        return 0;
-    }
+void enqueue( struct Queue q , int x ){
+    
+    if ( q.rear ==q.size-1 ) cout << "queue is full" <<endl;
+    
+    q.rear++;
+    q.Q[ q.rear ] = x;
     
 }
 
-void push( Stack *st , int value ){
-    if ( st->top == st->size-1 ) {
-        cout << "Stack overflow" << endl;
-    }
-    else{
-        st->top++;
-        st->s[st->top]=value;
-    }
-}
-
-void create( Stack *st ){
-    printf( "Enter Size" );
-    scanf("%d\n",st->size);
+void dequeue ( struct Queue q ){
+    if ( q.front == q.rear ) cout  << "queue is empty" <<endl;
     
-    st->top=-1;
-    st->s = (int *)malloc(st->size*sizeof(int));
-    
-}
-
-void display ( Stack *st ){
-    int i;
-    
-    for ( i = st->top ; i >=0 ; i-- ){
-        cout << st->s[i];
-    }
-    
-}
-
-        
-int pop( Stack *st){
-    int x = 0;
-    if ( st->top == -1 ) cout << "Stack is empty" << endl;
-    x = st->s[st->top--];
-    
-    return x;
-}
-
-int stackTop( Stack *st ){
-    if ( st->top == -1 ) return -1;
-    
-    return st->s[st->top];
-}
-
-int isEmpty( Stack *st ){
-    if ( st->top == -1 ) return 1;
-    return 0;
-}
-
-int isFull( Stack *st ){
-    if ( st->top == st->size-1 ) return 1;
-    return 0;
-}
-
-int peek( Stack st , int pos ){
-    int x = -1;
-    
-    if ( st.top-pos+1 < 0 ) cout << "no you're doing it wrong fam";
-    
-    x = st.s[st.top-pos+1];
-    
-    return x;
 }
 
 int main(){
     
+    struct Queue q;
+    cout << "enter size" <<endl;
+    scanf( "%d\n" , &q.size );
+    q.Q = new int[ q.size ];
+    q.front = q.rear = -1;
     return 0;
 }
